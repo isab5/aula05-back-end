3a4c7c373f4d040d00076fab1f9d6029635ae549
Isabella Borin <isabellaborin.tds1.senai@gmail.com>
2024-08-29 17:09:02 -0300

    refactor: alterdoces routes structure

--- src/server.js
+++ src/server.js
@@ -9,24 +9,6 @@ const app = express()
 
 app.use(express.json())
 
-const guloseimas = [
-    {
-        id: 1,
-        nome: "Trufa",
-        preco: 8.50,
-    },
-    {
-        id: 2,
-        nome: "Brigadeiro",
-        preco: 5.0,
-    },
-    {
-        id: 3,
-        nome: "Bomba de chocolate",
-        preco:6.0,
-    }
-]
-
 const filmesMarcantes = [
     {
         id: 1001,
@@ -50,24 +32,6 @@ const filmesMarcantes = [
 
 app.get("/", (req, res) => {
     return res.status(200).send({ message: "Hello, World!" })
-})
-
-app.get("/doces", (req, res) => {
-    return res.status(200).send(guloseimas)
-})
-
-app.post("/doces", (req, res) => {
-    const {nome, preco} = req.body
-
-    const novoDoce = {
-        id: guloseimas.length + 1,
-        nome: nome,
-        preco: preco,
-    }
-
-    guloseimas.push(novoDoce)
-    
-    return res.status(201).send(guloseimas)
 })
 
 app.get("/filmes", (req, res) => {
