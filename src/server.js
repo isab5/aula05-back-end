f753afa822b8276cd00fe14787c80fcb8934a1b7
Isabella Borin <isabellaborin.tds1.senai@gmail.com>
2024-08-29 15:51:30 -0300

    feat: add some requests

--- src/server.js
+++ src/server.js
@@ -9,8 +9,55 @@ const app = express()
 
 app.use(express.json())
 
+const guloseimas = [
+    {
+        id: 1,
+        nome: "Trufa",
+        preco: 8.50,
+    },
+    {
+        id: 2,
+        nome: "Brigadeiro",
+        preco: 5.0,
+    },
+    {
+        id: 3,
+        nome: "Bomba de chocolate",
+        preco:6.0,
+    }
+]
+
+const filmesMarcantes = [
+    {
+        id: 1001,
+        titulo: "Homem de ferro",
+        genero: "Ação",
+        emCartaz: false
+    },
+    {
+        id: 1002,
+        titulo: "Shrek",
+        genero: "Animação",
+        emCartaz: false
+    },
+    {
+        id: 1003,
+        titulo: "Deadpool & Wolverine",
+        genero: "Ação",
+        emCartaz: true
+    }
+]
+
 app.get("/", (req, res) => {
     return res.status(200).send({ message: "Hello, World!" })
+})
+
+app.get("/doces", (req, res) => {
+    return res.status(200).send(guloseimas)
+})
+
+app.get("/filmes", (req, res) => {
+    return res.status(200).send(filmesMarcantes)
 })
 
 app.listen(port, () => {
